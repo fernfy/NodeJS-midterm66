655585c935fd8dd1c99ec70663a17ee490b2b1e6
Yanisa Dokmithes <112007032+fernfy@users.noreply.github.com>
2023-08-23 14:06:03 +0700

    NodeJS-midterm66

---
+++ server/src/Midterm66.js
@@ -0,0 +1,17 @@
+module.exports ={
+
+ 
+
+    add1 (req,res){
+
+        input = parseInt(JSON.stringify(req.body['input']))
+
+        console.log('input = '+ input)
+
+        output = input + 1
+
+        res.send(output.toString())
+
+    },
+
+}
---
+++ server/src/que2.js
@@ -0,0 +1,5 @@
+function sum(x, x) {
+    return x  + x;
+}
+
+console.log("x + x = " + sum(x, x));
